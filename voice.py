b51cc5fc804bd4a1a9f7255ffc3b08462d84a036
ronakye <ronakde84@gmail.com>
2024-11-03 14:35:49 +0000

    dw

--- voice.py
+++ voice.py
@@ -22,19 +22,6 @@ def winrun():
     import os
     os.system("open voice/win.mp3")
 
-def sleekrun():
-    for i in ["wow","lol"]:
-        text = i
-        output_file = f"voice/{i}.mp3"
-        voice = "en-GB-RyanNeural"
-        async def mainye():
-            communicate = edge_tts.Communicate(text, voice)
-            await communicate.save(output_file)
-        asyncio.run(mainye())
-        import os
-        os.system(f"open voice/{i}.mp3")
-
-
 def ignorethisrun(): 
     for i in ["wow","lol"]:
         text = i
@@ -46,12 +33,6 @@ def ignorethisrun():
         asyncio.run(mainye())
         import os
         os.system(f"open voice/{i}.mp3") 
-
-
-def win():
-    import os
-    os.system("open voice/win.mp3")
-
 def ignorethis():
     import random
     import os
@@ -59,6 +40,13 @@ def ignorethis():
     if chance == 0:
         i=random.randint(0,1)
         os.system(f"open voice/{i}.mp3")
+
+
+
+def win():
+    import os
+    os.system("open voice/win.mp3")
+
 
 def say(stringyeye):
     text = f"{stringyeye}"
