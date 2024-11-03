565165828a9b75a8a90b4b30f4308e73a662eb02
FinleyStirk06 <FinleyStirk06@gmail.com>
2024-11-03 15:05:59 +0000

    modified:   chessSolver.py 	modified:   main.py 	modified:   voice/voiceoutput.mp3

--- chessSolver.py
+++ chessSolver.py
@@ -6,9 +6,11 @@ def BestMove(fenString):
     board = chess.Board(fen=fenString)
     engine = chess.engine.SimpleEngine.popen_uci("stockfish/stockfish-macos-m1-apple-silicon")
     try:
+        isWin = board.is_checkmate()
         move = engine.play(board, chess.engine.Limit(time=0.5)).move
     except:
         move = chess.Move.null
+        isWin = False
         print('invalid FEN')
     print(board)
-    return move
+    return (move, isWin)
